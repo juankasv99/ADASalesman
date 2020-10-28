0ca9bfbb227ccddc51850f1b8c55d0aff654e5ec
Juan Carlos Soriano Valle <thelakegray@gmail.com>
2020-10-28 23:26:20 +0100

    dijsktra puro + queue implementado y funcionando

---
+++ SalesMan V1/GraphApplication/GraphApplication/Greedy.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "Graph.h"
+
+// SalesmanTrackGreedy =========================================================
+
+CTrack SalesmanTrackGreedy(CGraph& graph, CVisits &visits)
+{
+	return CTrack(&graph);
+}
